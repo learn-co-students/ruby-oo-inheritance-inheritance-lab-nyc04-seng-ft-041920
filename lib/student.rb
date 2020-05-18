8c0c04754942052a1449af55ba631680544e3941
JonTDean <JonathanTomDean@Gmail.com>
2020-05-18 00:44:44 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  
+  attr_accessor = :knowledge
 
+
+  def initialize
+    super
+    self.knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
 end
