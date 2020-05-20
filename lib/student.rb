bec22e66a9f832d02538554fd39e9cda5f2b2939
github username <github email address>
2020-05-20 12:57:03 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize()
+    @knowledge = []
+  end
+
+  def learn(subject)
+    @knowledge << subject
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
